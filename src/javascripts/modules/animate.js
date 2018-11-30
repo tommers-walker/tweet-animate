920e97fc6368213c86569445e390f5e3651fffc5
Tom Walker <tom.w@niceandserious.com>
2018-11-30 11:24:12 +0000

    Tidy up

--- src/javascripts/modules/animate.js
+++ src/javascripts/modules/animate.js
@@ -2,13 +2,27 @@ import 'gsap';
 
 const animate = {
   init() {
-    TweenMax.to('#wing', 0.05, {
-      rotationY: "30",
+    const tweetIcon = document.querySelector('#tweet-icon');
+    const tl = new TimelineMax({paused:true});
+
+    tl
+    .to('#bird-wing', 0.05, {
+      rotationY: "30%",
       transformOrigin: "top right",
-      repeat: 15,
+      repeat: 9,
       yoyo: true,
       ease: Power0.easeNone
     })
+    .to(tweetIcon, 0.1, {
+      y: "-50%",
+      repeat: 1,
+      yoyo: true,
+      ease: Power1.easeOut
+    }, 0)
+
+    tweetIcon.addEventListener('mouseenter', function(e) {
+      tl.restart();
+    });
   }
 }
 
